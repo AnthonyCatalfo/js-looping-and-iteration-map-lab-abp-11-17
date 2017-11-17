6e197348bd2d37b027085284b21ca3a1dfdfe4f6
Anthony Catalfo <anthonycatalfo@gmail.com>
2017-11-17 05:42:47 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solution in this file.
+function lowerCaseDrivers(drivers){
+return drivers.map((driver) => {
+    return driver.toLowerCase()})
+
+}
+function splitFullName (fullName){
+let firstName = fullName.split(' ').slice(0, -1).join(' ');
+let lastName = fullName.split(' ').slice(-1).join(' ');
+let firstLast = {firstName:firstName, lastName:lastName};
+return firstLast;
+}
+function nameToAttributes(drivers){
+return drivers.map(splitFullName);
+}
+function attributesToPhrase(drivers){
+return drivers.map((driver) => {
+    return driver.name +" is from "+driver.hometown})
+  }
